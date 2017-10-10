1e485015041e70d193e2691def5854a0d002f876
jennyjiang95 <jennyjiang95@berkeley.edu>
2017-10-10 12:02:14 -0700

    add forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,37 @@ class PagesController < ApplicationController
         @cats = Cat.all
         @users = User.all
         @todos = Todo.all
+        @todo = Todo.new
+        @user = User.new
     end
 
     def new
     end
 
     def create
+    	#create the todos
+    	Todo.create(todo_params) 
+    	#STILL render a page, redirect to home page 
+    	redirect_to '/'
     end
+
+    def create_user
+    	    User.create(user_params)
+
+    	    redirect_to '/'
+
+    end
+
+
+
+		def user_params
+			params.require(:user).permit(:name, :email, :age)
+		end
+
+
+	private 
+		def todo_params
+			params.require(:todo).permit(:tasks, :finished)
+		end
+
 end
